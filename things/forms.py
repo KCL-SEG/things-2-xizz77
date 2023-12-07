8873e71b7ae75865868629950fce180d493708b7
Ziyad <z1927za@gmail.com>
2023-12-07 21:07:23 +0000

    Add solution to Things2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+
+from .models import Thing
+from django.core.validators import RegexValidator
+
+class ThingForm(forms.ModelForm):
+  class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea(),'quantity': forms.NumberInput()}
+        
+
+
--- things/views.py
+++ things/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from .forms import ThingForm
 def home(request):
+    form = ThingForm()
+    if request.method == "Post":
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
     return render(request, 'home.html', {'form': form})
